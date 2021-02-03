349135df464baac1ba1333ab1ae01e05cabf6dff
yiming <ymxia@bouffalolab.com>
2021-02-03 14:15:28 +0800

    [DIY] add clone custome packet before update seed

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.154/g' package/base-files/files/bin/config_generate
-git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok
-git clone https://github.com/kenzok8/small.git package/small
